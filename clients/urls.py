b1c4125c54ad607c58f6a421189d354593d3f9b2
vibhav <vibhav.1507@gmail.com>
2024-03-21 16:56:33 +0530

    add update and delete views

--- clients/urls.py
+++ clients/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('clients/', views.ClientListAPIView.as_view(), name='client-list'),
-    path('clients/<int:pk>/', views.ClientDetailAPIView.as_view(), name='client-detail'),
+    path('clients/edit/', views.ClientDetailAPIView.as_view(), name='client-detail'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -7,15 +7,19 @@ from django.http import Http404
 from rest_framework import permissions
 from users.auth import TokenAuthentication
 import requests
+from rest_framework import generics
 
     
 
 class ClientListAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [TokenAuthentication]
+    serializer_class = ClientSerializer
 
+    def get_queryset(self):
+        return Client.objects.filter(user=self.request.user)
+    
     def get_interface_info(self, interface_name, client_ip, client_port):
-        # Assuming the URL for querying interface info follows the format: http://clientIP:clientPort/device/wifi/interface/info?interfacename=interface
         url = f"http://{client_ip}:{client_port}/device/wifi/interface/info?interfacename={interface_name}"
         try:
             response = requests.get(url)
@@ -34,25 +38,27 @@ class ClientListAPIView(APIView):
             client.save()
             return True
         else:
+            client = Client.objects.get(clientIP=client_ip, clientPort=client_port)
             setattr(client, 'status', False)
         return False
+    
+    
     def get(self, request):
-        clients = Client.objects.filter(user=request.user)
-        #update interface info
+        clients = self.get_queryset()
+        if not clients:
+            return Response({"message": "No clients found"}, status=status.HTTP_404_NOT_FOUND)
         for client in clients:
             self.update_interface_info(client.clientIP, client.clientPort, client.interface)
-
-        serializer = ClientSerializer(clients, many=True)
+        serializer = self.serializer_class(clients, many=True)
         return Response(serializer.data)
 
+    
     def post(self, request):
-        serializer = ClientSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             client_ip = request.data.get('clientIP')
             client_port = request.data.get('clientPort')
             interface_name = request.data.get('interface')
-
-            # Save initial data
             serializer.save()
             interface_info = self.get_interface_info(interface_name, client_ip, client_port)
             if interface_info:
@@ -66,31 +72,47 @@ class ClientListAPIView(APIView):
                 return Response({"message": "Initial data saved but failed to fetch the rest of the info"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class ClientDetailAPIView(APIView):
 
+
+class ClientDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [TokenAuthentication]
+    serializer_class = ClientSerializer
 
-    def get_object(self, pk):
+    def get_object(self, id):
         try:
-            return Client.objects.get(pk=pk)
+            return Client.objects.get(id=id)
         except Client.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk):
-        client = self.get_object(pk)
-        serializer = ClientSerializer(client)
+    def get(self, request):
+        id = request.query_params.get('id')
+        client = self.get_object(id)
+        serializer = self.serializer_class(client)
         return Response(serializer.data)
-
-    def put(self, request, pk):
-        client = self.get_object(pk)
-        serializer = ClientSerializer(client, data=request.data)
+    
+    def put(self, request):
+        id = request.query_params.get('id')
+        client = self.get_object(id)
+        serializer = self.serializer_class(client, data=request.data)
         if serializer.is_valid():
+            client_ip = request.data.get('clientIP')
+            client_port = request.data.get('clientPort')
+            interface_name = request.data.get('interface')
             serializer.save()
-            return Response(serializer.data)
+            interface_info = ClientListAPIView.get_interface_info(self, interface_name, client_ip, client_port)
+            if interface_info:
+                for key, value in interface_info.items():
+                    setattr(client, key, value)
+                setattr(client, 'status', True)
+                client.save()
+                return Response(serializer.data)
+            else:
+                return Response({"message": "Data updated but failed to fetch the rest of the info"}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        client = self.get_object(pk)
+    def delete(self, request):
+        id = request.query_params.get('id')
+        client = self.get_object(id)
         client.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
